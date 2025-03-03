5224c3b97552ba8e0650f860b4855dc6af81158c
soyacen <soyacen@outlook.com>
2025-03-03 15:28:27 +0800

    feat: add application package

--- wire.go
+++ wire.go
@@ -2,21 +2,17 @@ package app_layout
 
 import (
 	"github.com/go-leo/app-layout/adapter"
+	"github.com/go-leo/app-layout/application"
 	"github.com/go-leo/app-layout/assembler"
-	"github.com/go-leo/app-layout/command"
 	"github.com/go-leo/app-layout/model"
-	"github.com/go-leo/app-layout/query"
-	"github.com/go-leo/app-layout/service"
 	"github.com/go-leo/app-layout/transport"
 	"github.com/google/wire"
 )
 
 var Provider = wire.NewSet(
 	transport.Provider,
+	application.Provider,
 	assembler.Provider,
-	command.Provider,
-	query.Provider,
-	service.Provider,
 	adapter.Provider,
 	model.Provider,
 )
